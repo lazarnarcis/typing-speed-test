c78b429911f10a4bde605434022b93834531cf1b
Lazar Narcis <lnarcis310@gmail.com>
2022-06-07 23:03:40 +0300

    new updates

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let div = document.querySelector("#speedTest");
-let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat ea sed, tenetur dolor cumque ullam fugiat magnam natus deserunt aperiam perspiciatis libero quaerat, iusto nemo laborum harum, reprehenderit officiis consequatur.";
+let text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo beatae adipisci distinctio aspernatur omnis temporibus eveniet excepturi nostrum deserunt, quia perferendis totam impedit animi corrupti voluptas hic nesciunt doloribus expedita!";
 let time = 60;
 let timeHTML = document.querySelector("#time");
 let writeHere = "";
 text = text.toLowerCase();
 let pos = 0;
 let initialLetter = text[0];
+let textToShow = document.querySelector("#textToShow");
 
 function showTime () {
     if (time == 0) {
-        timeHTML.innerText = "Time is up!";
+        timeHTML.innerText = "Time is up or you're done!";
         return;
     }
     timeHTML.innerText = time + "s";
@@ -26,16 +27,19 @@ div.appendChild(paragraph);
 
 document.body.addEventListener("keyup", function (e) {
     if (time == 0) {
-        alert("Time is up!");
+        alert("Time is up or you're done!");
     } else {
         let key = e.key.toLowerCase();
 
         if (key == initialLetter) {
-            paragraph.innerHTML = text.replaceAt(pos, `<span style='background-color: red;'>${key}</span>`);
-            console.log(initialLetter);
-            console.log(pos);
+            textToShow.innerText += initialLetter;
             pos++;
             initialLetter = text[pos];
+
+            if (textToShow.innerText == text) {
+                alert(`Good job! You have done in ${time} seconds!`);
+                time = 0;
+            }
         }
     }
 });
